96a81109b42e60b63e36bd491777bfdb052edd70
jscudweiss <jscudereWeiss@gmail.com>
2024-04-03 12:12:10 -0400

    lab_complete?

--- django/djangoproject/djangoproject/urls.py
+++ django/djangoproject/djangoproject/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', lambda req: redirect('admin/')),
     path('minifacebook/', include('minifacebook.urls')),
+    #path('university/', include('university.urls')),
     path('admin/', admin.site.urls),
 ]
--- django/djangoproject/university/admin.py
+++ django/djangoproject/university/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student, Room, Course
+
+admin.site.register(Student)
+admin.site.register(Room)
+admin.site.register(Course)

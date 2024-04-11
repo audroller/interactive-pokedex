fd0d69f004aec239158f256ab7f532681aabb70f
Iridantic <odd.rolls@gmail.com>
2024-04-11 17:29:27 -0400

    Update models.py

--- django/djangoproject/pokedex/models.py
+++ django/djangoproject/pokedex/models.py
@@ -44,3 +44,11 @@ class Ability(models.Model):
         
     def __str__(self):
         return f"<ID: {self.abilityID} Name: {self.name} Desc: {self.affect}>"
+    
+class User(models.Model):
+    userid=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username=models.TextField()
+    ranking=models.ManyToManyField(Pokemon)
+
+    def __str__(self):
+        return f"<ID: {self.userid} Username: {self.username}>"

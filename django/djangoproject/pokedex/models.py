61d71e38bb2ccf5656692d69e0d27cf798cd05a2
jscudweiss <jscudereWeiss@gmail.com>
2024-04-11 18:48:48 -0400

    pokedex

--- django/djangoproject/pokedex/models.py
+++ django/djangoproject/pokedex/models.py
@@ -17,6 +17,17 @@ class EleType(models.Model):
     def __str__(self):
         return f"<name: {self.Name}>"
 
+class Ability(models.Model):
+    abilityID = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    affect = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Abilities"
+        
+    def __str__(self):
+        return f"<ID: {self.abilityID} Name: {self.name} Desc: {self.affect}>"
+
 class Pokemon(models.Model):
     number = models.IntegerField(primary_key=True)
     name = models.TextField()
@@ -26,6 +37,7 @@ class Pokemon(models.Model):
     weight = models.IntegerField()
     PrevEvolution = models.ForeignKey("self", on_delete=models.CASCADE)
     Type = models.ManyToManyField(EleType)
+    Abilities = models.ManyToManyField(Ability)
 
     class Meta:
         verbose_name_plural = "Pokemon"
@@ -33,23 +45,11 @@ class Pokemon(models.Model):
     def __str__(self):
         return f"<ID: {self.number} Name: {self.name} Types: >"
     
-
-
-class Ability(models.Model):
-    abilityID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.TextField()
-    affect = models.TextField()
-
-    class Meta:
-        verbose_name_plural = "Abilities"
-        
-    def __str__(self):
-        return f"<ID: {self.abilityID} Name: {self.name} Desc: {self.affect}>"
     
 class User(models.Model):
     userid=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username=models.TextField()
-    ranking=models.ManyToManyField(Pokemon)
+    favorites=models.ManyToManyField(Pokemon)
 
     def __str__(self):
         return f"<ID: {self.userid} Username: {self.username}>" 

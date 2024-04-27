7f456296e46a2a642f8d25151aa02f868defbd8f
chlohal <email@clh.sh>
2024-04-26 21:41:39 -0400

    Ensure all tables are editable

--- django/djangoproject/pokedex/admin.py
+++ django/djangoproject/pokedex/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pokemon, EleType, Ability, User
+from .models import Pokemon, EleType, Ability, User, PokemonImage
 
 admin.site.register(Pokemon)
+admin.site.register(PokemonImage)
 admin.site.register(EleType)
 admin.site.register(Ability)
 admin.site.register(User)
